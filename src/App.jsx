26b3919b03a66c13d10e35b6c763ab20c5c15cd0
khinenweoo <devkhinenweoo@gmail.com>
2025-12-19 02:19:34 +0700

    fix: typo error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Header"
-import ProcessFlow from "./components/Processflow"
+import ProcessFlow from "./components/ProcessFlow"
 import Projects from "./components/ProjectsSection"
 import TestimonialSection from "./components/TestimonialSection"
 import ContactSection from "./components/ContactSection"
